d0329804aa67a9e6dffa0aa684bc7fd099bedb71
DY <dimaudin404@gmail.com>
2026-05-15 18:25:04 +0300

    исправил неправильное расположение кнопок

--- Keyboards/keyboard.py
+++ Keyboards/keyboard.py
@@ -2,7 +2,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 main = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='/help')],
-    [KeyboardButton(text='/menu')], [KeyboardButton(text='/promo')]
-    ],
+    [KeyboardButton(text='/menu'), KeyboardButton(text='/promo')]
+],
     resize_keyboard=True
 )
