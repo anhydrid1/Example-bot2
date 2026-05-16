bcfa8383f0475336d5e8a37158da14a15c42f6b8
DY <dimaudin404@gmail.com>
2026-05-16 14:41:07 +0300

    fs

--- handlers/basic_handlers.py
+++ handlers/basic_handlers.py
@@ -14,12 +14,21 @@ async def cmd_start(message: types.Message):
             'Отправь мне фото, аудио или стикер.',
             reply_markup=kb.main)
 
+
 @basic_router.callback_query(F.data == 'help')
 async def help_cmd(callback: CallbackQuery):
     await callback.answer('Вы открыли список команд')
-    await callback.message.edit_text('dfgh')
 
+    text = (
+        '<b>Справка по командам бота:</b>\n\n'
+        '<b>/start</b> - <i>начало работы с ботом</i>\n'
+        '<b>/send_photo</b> - <i>прислать тестовую картинку</i>\n'
+        '<b>/send_doc</b> - <i>прислать тестовый документ</i>\n'
+        '<b>/menu</b> - <i>показать меню возможностей</i>\n'
+        '<b>/promo</b> - <i>получить акцию</i>\n'
+        '<b>/card</b> - <i>получить открытку</i>\n')
 
+    await callback.message.answer(text)
 
 @basic_router.message(F.text, lambda message: "дурак" in message.text.lower())
 async def rule(message: types.Message):
