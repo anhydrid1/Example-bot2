0383b0a338adf827f47a962cc190091b5055fbd9
DY <dimaudin404@gmail.com>
2026-05-16 14:37:47 +0300

    asdf

--- handlers/basic_handlers.py
+++ handlers/basic_handlers.py
@@ -16,22 +16,9 @@ async def cmd_start(message: types.Message):
 
 @basic_router.callback_query(F.data == 'help')
 async def help_cmd(callback: CallbackQuery):
+    await callback.answer('Вы открыли список команд')
+    await callback.message.edit_text('dfgh')
 
-    text = (
-        '<b>Справка по командам бота:</b>\n\n'
-        '<b>/start</b> - <i>начало работы с ботом</i>\n'
-        '<b>/send_photo</b> - <i>прислать тестовую картинку</i>\n'
-        '<b>/send_doc</b> - <i>прислать тестовый документ</i>\n'
-        '<b>/menu</b> - <i>показать меню возможностей</i>\n'
-        '<b>/promo</b> - <i>получить акцию</i>\n'
-        '<b>/card</b> - <i>получить открытку</i>\n'
-        '<b>/cars</b> - <i>список машин</i>'
-    )
-
-    await callback.message.edit_text(text, parse_mode='HTML',
-                                     reply_markup=await kb.inline_cars())
-
-    await callback.answer('Вы открыли список комманд')
 
 
 @basic_router.message(F.text, lambda message: "дурак" in message.text.lower())
